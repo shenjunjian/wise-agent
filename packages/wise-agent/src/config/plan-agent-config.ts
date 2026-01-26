3c6dcc4821d20e3332bb50d1608ec33c6f749c5e
shenjunjian <40288193@qq.com>
2026-01-26 15:17:08 +0800

    1

--- packages/wise-agent/src/config/plan-agent-config.ts
+++ packages/wise-agent/src/config/plan-agent-config.ts
@@ -3,15 +3,15 @@ import { createOpenAI } from "@ai-sdk/openai";
 
 export const planAgentConfig: AgentConfig[] = [
   {
-    name: "qwen3-max",
+    name: "qwen-flash",
     aiProvider: createOpenAI({
       apiKey: import.meta.env.VITE_QWEN_APIKEY,
       baseURL: import.meta.env.VITE_QWEN_BASEURL,
     }),
     tools: {},
     description:
-      "通义千问3系列Max模型，相较preview版本在智能体编程与工具调用方向进行了专项升级。本次发布的正式版模型达到领域SOTA水平，适配场景更加复杂的智能体需求。",
-    price: `input: 3.2 元/百万\n output: 12.8 元/百万`,
+      "Qwen3系列Flash模型，实现思考模式和非思考模式的有效融合，可在对话中切换模式。复杂推理类任务性能优秀，指令遵循、文本理解等能力显著提高。支持1M上下文长度，按照上下文长度进行阶梯计费",
+    price: `input: 0.15 元/百万\n output: 1.5 元/百万`,
   },
   {
     name: "qwen-plus",
@@ -25,14 +25,14 @@ export const planAgentConfig: AgentConfig[] = [
     price: `input: 0.8 元/百万\n output: 2 元/百万`,
   },
   {
-    name: "qwen-flash",
+    name: "qwen3-max",
     aiProvider: createOpenAI({
       apiKey: import.meta.env.VITE_QWEN_APIKEY,
       baseURL: import.meta.env.VITE_QWEN_BASEURL,
     }),
     tools: {},
     description:
-      "Qwen3系列Flash模型，实现思考模式和非思考模式的有效融合，可在对话中切换模式。复杂推理类任务性能优秀，指令遵循、文本理解等能力显著提高。支持1M上下文长度，按照上下文长度进行阶梯计费",
-    price: `input: 0.15 元/百万\n output: 1.5 元/百万`,
+      "通义千问3系列Max模型，相较preview版本在智能体编程与工具调用方向进行了专项升级。本次发布的正式版模型达到领域SOTA水平，适配场景更加复杂的智能体需求。",
+    price: `input: 3.2 元/百万\n output: 12.8 元/百万`,
   },
 ];
